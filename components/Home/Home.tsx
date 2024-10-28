ed6149b573ec63c4cf0c1494e5cb8719834c871f
Parham <parham.pourkhani23@gmail.com>
2024-10-28 18:56:36 +0330

    Add new part

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Hero from "./Hero";
+import Category from "./Category";
+import AllProduct from "./AllProduct";
 
 const Home = () => {
   return <div>
     <Hero/>
+    <Category/>
+    <AllProduct/>
   </div>;
 };
 
---
+++ components/Home/ProductCard.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { Product } from "@/typing";
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  product: Product;
+};
+
+const ProductCard = ({ product }: Props) => {
+  const num = Math.round(product.rating.rate);
+  const ratingArray = new Array(num).fill(0);
+
+  return (
+    <div className="p-4">
+      <div className="w-[200px] h-[150px]">
+        <Image
+          src={product.image}
+          alt={product.title}
+          width={100}
+          height={100}
+          className="w-[80%] h-[80%] object-contain"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
