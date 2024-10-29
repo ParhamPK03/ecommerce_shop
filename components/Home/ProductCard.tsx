fe003c2f1062fe36c3b1fcfa10702c71975d06d1
Parham <parham.pourkhani23@gmail.com>
2024-10-29 11:46:38 +0330

    .

--- components/Home/ProductCard.tsx
+++ components/Home/ProductCard.tsx
@@ -24,14 +24,17 @@ const ProductCard = ({ product }: Props) => {
           className="w-[80%] h-[80%] object-contain"
         />
       </div>
+      {/* Category */}
       <p className="mt-5 text-xs capitalize text-gray-600">
         {product.category}
       </p>
+      {/* Title */}
       <Link href={`/product/product-details/${product.id}`}>
         <h1 className="text-lg cursor-pointer hover:text-blue-900 transition-all hover:underline sm:w-full sm:truncate mt-2 text-black font-semibold">
           {}product.title
         </h1>
       </Link>
+      {/* Rating  */}
       <div className="flex items-center">
         {ratingArray.map((star) => {
           return (
